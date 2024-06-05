84847d0aa60a4fdf7b5df3ce9e0967a7a33a290b
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-05 22:15:28 +0800

    feat: add guest and user layout
    This layouts will be used to easily shift from guest mode to user mode
    and vice versa.

---
+++ src/components/GuestLayout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<div class="flex flex-col min-h-screen">
+  <main class="flex-grow">
+    <slot />
+  </main>
+</div>
---
+++ src/components/UserLayout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import "../app.css";
+  import NavBar from "$components/NavBar.svelte";
+  import { Toaster } from "svelte-french-toast";
+</script>
+
+<Toaster />
+<div class="flex flex-col min-h-screen">
+  <NavBar />
+  <main class="flex-grow">
+    <slot />
+  </main>
+  <footer
+    class="footer footer-center p-4 bg-base-300 text-base-content absolute inset-x-0 bottom-0"
+  >
+    <aside>
+      <p>Project Rapport - Copyright © 2024</p>
+    </aside>
+  </footer>
+</div>
