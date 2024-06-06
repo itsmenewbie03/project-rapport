82b0f2963f74ff408c7bc37720f7f8beed547d22
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-06 14:53:59 +0800

    wip: implement auth in backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,61 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::Serialize;
+use crate::auth::User;
 
-pub mod ngl;
-pub mod utils;
-
-#[derive(Serialize)]
-struct SpamResult {
-    message: String,
-    success: u8,
-    failed: u8,
-}
+pub mod auth;
 
 #[tauri::command]
-async fn spam(
-    target: &str,
-    count: u8,
-    random: bool,
-    message: Option<&str>,
-) -> Result<SpamResult, ()> {
-    let mut success_count = 0;
-    let mut fail_count = 0;
-
-    if !random && message.is_none() {
-        return Ok(SpamResult {
-            message: "Random Message is off but you did not provide a message to send!".to_owned(),
-            success: 0,
-            failed: 0,
-        });
-    }
-
-    for _x in 0..count {
-        let msg = if random {
-            utils::get_random_message()
-        } else {
-            // NOTE: message can't be none here since we have a check above
-            message.unwrap().to_owned()
-        };
-        let success = ngl::send(target, &msg).await;
-        if success {
-            success_count += 1;
-            continue;
-        }
-        fail_count += 1;
-    }
-    println!("SPAM COMPLETE\nSuccess: {success_count}\nFailure: {fail_count}");
-    Ok(SpamResult {
-        message: "Spam Complete!".to_owned(),
-        success: success_count,
-        failed: fail_count,
+async fn authenticate(email: &str, password: &str) -> Result<User, ()> {
+    println!("[RUST]: authenticatation for {}:{}", email, password);
+    // TODO: perform logic
+    Ok(User {
+        name: "John Doe".to_owned(),
+        id: 1,
+        email: "johndoe@example.com".to_owned(),
     })
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![spam])
+        .invoke_handler(tauri::generate_handler![authenticate])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
