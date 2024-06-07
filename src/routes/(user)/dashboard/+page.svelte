08544fc752c97cc35e6102348753e682deeb24ae
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-07 13:43:12 +0800

    feat: add logout button to the dashboard route

--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
+  import { signOut } from "@auth/sveltekit/client";
   import { page } from "$app/stores";
+  import UserLayout from "$components/UserLayout.svelte";
 </script>
 
-{#if $page.data.session}
-  <h1>Protected page</h1>
-  <p>
-    This is a protected content. You can access this content because you are
-    signed in.
-  </p>
-  <p>Session expiry: {$page.data.session?.expires}</p>
-{:else}
-  <h1>Access Denied</h1>
-{/if}
+<UserLayout>
+  {#if $page.data.session}
+    <h1 class="text-4xl">Protected page</h1>
+    <p class="text-md">
+      This is a protected content. You can access this content because you are
+      signed in.
+    </p>
+    <p class="text-sm">Session expiry: {$page.data.session?.expires}</p>
+
+    <button
+      on:click={(_e) => {
+        signOut();
+      }}
+      class="btn btn-primary"
+    >
+      LOGOUT!
+    </button>
+  {:else}
+    <h1>Access Denied</h1>
+  {/if}
+</UserLayout>
