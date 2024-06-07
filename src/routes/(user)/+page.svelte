662e6c8d546f635cfa1fbd107d627fc344c9997a
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-07 13:39:31 +0800

    feat: show a loading indicator in root route
    We will show a loading indicator until the onMount function is invoked.
    This is to ensure we are not showing anything until the authentication
    is completed. We will also show a message in case of redirection failure
    telling the user that something happened and the redirection failed.

--- src/routes/(user)/+page.svelte
+++ src/routes/(user)/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
   import GuestLayout from "$components/GuestLayout.svelte";
   import { page } from "$app/stores";
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  let loaded: boolean = false;
+  onMount(async () => {
+    loaded = true;
+    if ($page.data.session) {
+      await goto("/dashboard");
+    }
+  });
 </script>
 
 <GuestLayout>
-  <div>
-    {#if !$page.data.session}
-      <p>Not logged in!</p>
-    {/if}
-
-    {#if $page.data.session}
-      <p>Logged in!</p>
-      {JSON.stringify($page.data.session)}
+  <div class="flex flex-col min-h-screen justify-center items-center">
+    {#if loaded}
+      {#if !$page.data.session}
+        <p>
+          For some reason you are not redirected to the login route. Please
+          reload the page. You should not be able to see this as if you don't
+          have a session you will be redirected to the login page.
+        </p>
+      {/if}
+    {:else}
+      <span class="loading loading-bars loading-lg"></span>
     {/if}
   </div>
 </GuestLayout>
