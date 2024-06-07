596ef7230a69558c2664c877a7289bdd5e933238
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-07 13:17:44 +0800

    feat: add authentication feature
    This fully implements the authentication feature. This is done by
    comparing the credentials provided to the data stored in the local
    sqlite database. We then return an Optional user data where null means
    the authentication failed.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use crate::auth::User;
+pub mod utils;
 
-pub mod auth;
+use utils::auth::is_credentials_valid;
+use utils::auth::models::UserData;
+use utils::db;
 
 #[tauri::command]
-async fn authenticate(email: &str, password: &str) -> Result<User, ()> {
+async fn authenticate(email: &str, password: &str) -> Result<Option<UserData>, ()> {
     println!("[RUST]: authenticatation for {}:{}", email, password);
-    // TODO: perform logic
-    Ok(User {
-        name: "John Doe".to_owned(),
-        id: 1,
-        email: "johndoe@example.com".to_owned(),
-    })
+    db::init().await;
+    if is_credentials_valid(email, password).await {
+        // NOTE: we can safely unwrap this
+        // as we do have valid credentials
+        let user_data = db::get_user(email).await.unwrap();
+        Ok(Some(UserData {
+            name: user_data.name,
+            id: user_data.id,
+            email: user_data.email,
+        }))
+    } else {
+        Ok(None)
+    }
 }
 
 fn main() {
