208b287f0c1093e514144fb8c39698aa5b64cdac
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-07 13:22:12 +0800

    feat: add Toaster component to GuestLayout

--- src/components/GuestLayout.svelte
+++ src/components/GuestLayout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import "../app.css";
+  import { Toaster } from "svelte-french-toast";
 </script>
 
-<div class="flex flex-col min-h-screen">
+<Toaster />
+<div class="flex flex-col min-h-screen bg-base-100">
   <main class="flex-grow">
     <slot />
   </main>
