a871bbd7301bc999c3af757adc2663f9bee228a6
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-08 22:37:46 +0800

    feat: add jwt module
    This module provides us an easy way to generate and validate
    jsonwebtokens. Also a helper function that returns an expiry date 24
    hours from the current timestamp.

---
+++ src-tauri/src/utils/jwt.rs
@@ -0,0 +1,66 @@
+use std::env;
+
+use dotenv;
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub exp: usize,
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+}
+
+pub fn generate(claims: Claims) -> Option<String> {
+    dotenv::dotenv().ok();
+    match env::var("AUTH_SECRET") {
+        Ok(secret) => {
+            match encode(
+                &Header::default(),
+                &claims,
+                &EncodingKey::from_secret(secret.as_ref()),
+            ) {
+                Ok(token) => Some(token),
+                Err(err) => {
+                    println!("[RUST]: failed to generate token\nERROR: {}", err);
+                    None
+                }
+            }
+        }
+        Err(err) => {
+            println!("[RUST]: failed to read env for the secret\nERROR: {}", err);
+            None
+        }
+    }
+}
+
+pub fn validate(token: &str) -> Option<Claims> {
+    dotenv::dotenv().ok();
+    match env::var("AUTH_SECRET") {
+        Ok(secret) => {
+            match decode::<Claims>(
+                token,
+                &DecodingKey::from_secret(secret.as_ref()),
+                &Validation::default(),
+            ) {
+                Ok(token_data) => {
+                    println!("[RUST]: token validation success");
+                    Some(token_data.claims)
+                }
+                Err(err) => {
+                    println!("[RUST]: failed to validate token\nERROR: {}", err);
+                    None
+                }
+            }
+        }
+        Err(err) => {
+            println!("[RUST]: failed to read env for the secret\nERROR: {}", err);
+            None
+        }
+    }
+}
+
+pub fn get_exp() -> u64 {
+    jsonwebtoken::get_current_timestamp() + 24 * 60 * 60
+}
