b91e6faf33865df7a4dcf8131131408f2c8bbcbb
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-09 11:57:26 +0800

    feat: make navigation bar functional
    This makes the navigation bar functional by updating the dropdown items
    to reflect our use case. We make the items point to their respective
    pages. We also added profile and logout dropdown items on the navbar end
    button which are both functional.

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+  import toast from "svelte-french-toast";
+  import { logout } from "$lib/utils";
+  import { goto } from "$app/navigation";
+  const _logout = async (event: Event) => {
+    await toast.promise(logout(), {
+      loading: "Signing you out...",
+      success: "You've been logged out successfully.",
+      error: "We encountered an issue logging you out. Please try again.",
+    });
+    await goto("/login");
+  };
+</script>
+
 <div class="navbar bg-base-100">
   <div class="navbar-start">
     <div class="dropdown">
@@ -17,51 +31,43 @@
         >
       </div>
       <ul
-        tabindex="0"
+        tabindex="-1"
         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
       >
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/analytics">Analytics</a></li>
+        <li><a href="/configurations">Configurations</a></li>
       </ul>
     </div>
   </div>
   <div class="navbar-center">
-    <a class="btn btn-ghost text-xl">NGL Spammer</a>
+    <p class="font-bold text-xl">Project Rapport</p>
   </div>
   <div class="navbar-end">
-    <button class="btn btn-ghost btn-circle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        /></svg
-      >
-    </button>
-    <button class="btn btn-ghost btn-circle">
-      <div class="indicator">
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
           fill="none"
           viewBox="0 0 24 24"
+          stroke-width="1.5"
           stroke="currentColor"
-          ><path
+          class="size-6"
+        >
+          <path
             stroke-linecap="round"
             stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-          /></svg
-        >
-        <span class="badge badge-xs badge-primary indicator-item"></span>
+            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
       </div>
-    </button>
+      <ul
+        tabindex="-1"
+        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li><a href="/profile">Profile</a></li>
+        <li><button on:click={_logout}>Logout</button></li>
+      </ul>
+    </div>
   </div>
 </div>
