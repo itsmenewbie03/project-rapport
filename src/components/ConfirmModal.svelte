bc7f34f9bc977d73c4a2c74f57f539d81daddf1f
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-11 12:35:24 +0800

    feat: add new component ConfirmModal
    This handles the prompt for the user when exiting feedback page.
    This contains all implementation including the password verification.
    Also inserting the flag to bypass the nav back cancel call.

---
+++ src/components/ConfirmModal.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import toast from "svelte-french-toast";
+  import Icon from "./Icon.svelte";
+  import { goto } from "$lib/utils";
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { page } from "$app/stores";
+
+  let handle: HTMLDialogElement;
+
+  const hide = () => {
+    handle.classList.remove("modal-open");
+  };
+
+  export const show = () => {
+    handle.classList.add("modal-open");
+  };
+
+  let password: string = "";
+
+  const _confirm = async (event: Event) => {
+    // TEST: we will implement the actual password verification tomorrow
+    event.preventDefault();
+    const is_verified = await invoke("authenticate", {
+      email: $page.data.session.email,
+      password: password,
+    });
+    if (is_verified) {
+      localStorage.setItem("allowed_to_exit", "true");
+      hide();
+      await toast.promise(goto("/dashboard"), {
+        loading: "Access granted!.",
+        success: "Welcome Back!",
+        error: "Failed to redirect, please reload the page.",
+      });
+      localStorage.removeItem("allowed_to_exit");
+    } else {
+      hide();
+      toast.error("Incorrect password. Please try again.");
+      password = "";
+    }
+  };
+</script>
+
+<dialog id="confirm_model" class="modal" bind:this={handle}>
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Authorized Access Only!</h3>
+    <p class="py-4">Please verify you indentity by entering your password.</p>
+    <form class="form-control" id="password_confirm_form" on:submit={_confirm}>
+      <label class="input input-bordered flex items-center gap-2">
+        <Icon name="password" />
+        <input type="password" class="grow" bind:value={password} required />
+      </label>
+    </form>
+    <div class="modal-action">
+      <button class="btn btn-primary" type="submit" form="password_confirm_form"
+        >Confirm</button
+      >
+      <button class="btn" on:click={hide}>Cancel</button>
+    </div>
+  </div>
+</dialog>
