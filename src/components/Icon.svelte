8fe1b948fd1f85ee71ee25e6c238c967fe5a268d
itsmenewbie03 <89230263+itsmenewbie03@users.noreply.github.com>
2024-06-13 21:48:16 +0800

    feat: make class and viewBox customizable (#11)
    This adds _class and viewBox property to the Icon component, this give
    us flexibility as we could resize the Icon instance.

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { get_icon } from "$lib/icons";
   export let name;
+  export let _class: string | undefined = undefined;
+  export let viewBox: string | undefined = undefined;
 </script>
 
-{@html get_icon(name)}
+{@html get_icon(name, _class, viewBox)}
