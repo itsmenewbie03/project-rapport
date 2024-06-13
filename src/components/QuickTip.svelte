535d83c0e9e98a543c4a38429ccad750d483dc5a
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-13 22:39:31 +0800

    feat: add new component QuickTip
    This component wraps the Helper dropdown component of daisyUI.

---
+++ src/components/QuickTip.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let hasTitle: boolean = false;
+</script>
+
+<div class="dropdown dropdown-hover dropdown-start">
+  <div class="badge badge-primary badge-outline ml-2">Quick Tip</div>
+  <div
+    tabindex="-1"
+    class="card compact dropdown-content z-[1] shadow bg-primary-content rounded-box w-64"
+  >
+    <div tabindex="-1" class="card-body">
+      {#if hasTitle}
+        <h2 class="card-title">{title}</h2>
+      {/if}
+      <p>{description}</p>
+    </div>
+  </div>
+</div>
