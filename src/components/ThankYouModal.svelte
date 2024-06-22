9af50b4c02254e5d1ac73499f4011792dab24e71
itsmenewbie03 <89230263+itsmenewbie03@users.noreply.github.com>
2024-06-22 13:19:49 +0800

    feat: thank you modal and guarantee emotion data completeness (#19)
    * feat: add new component ThankYouModal
    
    This replaces the `toast.success` we use to display our thank you
    message.
    
    closes #15
    
    * feat: use modal and prevent incomplete emotion data
    
    This uses the new component `ThankYouModal` to display the thank you
    message. Also implements a check to ensure all emotion data is resolved
    before allowing submission.
    
    closes #18
    
    * feat: stop analysis instead of clear recording
    
    We're not recording anymore so we will just stop the analysis when clear
    recording method is called. Too lazy to name them properly, they work
    and that's it.

---
+++ src/components/ThankYouModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let handle: HTMLDialogElement;
+
+  export const hide = () => {
+    handle.classList.remove("modal-open");
+  };
+
+  export const show = () => {
+    handle.classList.add("modal-open");
+  };
+
+  export let message: string;
+</script>
+
+<dialog id="confirm_model" class="backdrop-blur-md modal" bind:this={handle}>
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Thank You For Your Feedback!</h3>
+    <p class="py-4">{message}</p>
+  </div>
+</dialog>
