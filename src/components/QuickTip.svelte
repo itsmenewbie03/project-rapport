5a0c9fb77e0eff86f52a2d00e2d4ac4e0db6ea0d
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-24 19:45:31 +0800

    feat: use icon instead of quick tip text

--- src/components/QuickTip.svelte
+++ src/components/QuickTip.svelte
@@ -5,7 +5,26 @@
 </script>
 
 <div class="dropdown dropdown-hover dropdown-start">
-  <div class="badge badge-primary badge-outline ml-2">Quick Tip</div>
+  <div
+    tabindex="0"
+    role="button"
+    class="btn btn-circle btn-ghost btn-xs text-info"
+  >
+    <svg
+      tabindex="-1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      class="size-4 stroke-current"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+    </svg>
+  </div>
   <div
     tabindex="-1"
     class="card compact dropdown-content z-[1] shadow bg-primary-content rounded-box w-64"
