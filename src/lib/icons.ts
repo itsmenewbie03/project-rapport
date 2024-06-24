062a1c1934f7fdedb0044094828c3835aa9876f5
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-24 19:48:36 +0800

    feat: add happy and sad icon

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -39,6 +39,16 @@ const PAGE_ICONS: Record<string, string> = {
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="[viewBox]" stroke-width="1.5" stroke="currentColor" class="[class]">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
 </svg>`,
+  happy: `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="[viewBox]" stroke-width="1.5" stroke="currentColor" class="[class]">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+</svg>
+`,
+  sad: `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="[viewBox]" stroke-width="1.5" stroke="currentColor" class="[class]">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+</svg>
+`,
   default: `
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="[viewBox]" stroke-width="1.5" stroke="currentColor" class="[class]">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
@@ -46,16 +56,18 @@ const PAGE_ICONS: Record<string, string> = {
 };
 
 const load_class = (svg: string, cls: string, viewBox: string): string => {
-  return svg
-    .replace(/\[class\]/g, cls)
-    .replace(/\[viewBox\]/g, viewBox);
-}
+  return svg.replace(/\[class\]/g, cls).replace(/\[viewBox\]/g, viewBox);
+};
 
 /**
  * This will return an icon in svg that could be rendered
  * using the `{@html arg}` svelte directive
  * */
-const get_icon = (page: string, _class: string = "size-6", view_box: string = "0 0 24 24"): string => {
+const get_icon = (
+  page: string,
+  _class: string = "size-6",
+  view_box: string = "0 0 24 24",
+): string => {
   if (!Object.keys(PAGE_ICONS).includes(page)) {
     return load_class(PAGE_ICONS.default, _class, view_box);
   }
