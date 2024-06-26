0b97ab4093e3b7e74a1bb4ce7925dc38bdffb812
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-26 21:02:00 +0800

    feat: add model FeedbackDataRow

--- src-tauri/src/utils/models.rs
+++ src-tauri/src/utils/models.rs
@@ -6,3 +6,10 @@ pub struct ConfigData {
     pub name: String,
     pub value: String,
 }
+
+#[derive(Serialize, Clone, FromRow, Debug)]
+pub struct FeedbackDataRow {
+    pub id: i32,
+    pub data: String,
+    pub created_at: String,
+}
