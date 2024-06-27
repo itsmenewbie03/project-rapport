itsmenewbie03/project-rapport

commit 9af50b4c02254e5d1ac73499f4011792dab24e71

feat: thank you modal and guarantee emotion data completeness (#19)
* feat: add new component ThankYouModal

This replaces the `toast.success` we use to display our thank you
message.

closes #15

* feat: use modal and prevent incomplete emotion data

This uses the new component `ThankYouModal` to display the thank you
message. Also implements a check to ensure all emotion data is resolved
before allowing submission.

closes #18

* feat: stop analysis instead of clear recording

We're not recording anymore so we will just stop the analysis when clear
recording method is called. Too lazy to name them properly, they work
and that's it.

src/routes/(user)/feedback/form/+page.svelte
@@ -2,18 +2,23 @@
   import UserLayout from "$components/UserLayout.svelte";
   import LoadingBars from "$components/LoadingBars.svelte";
   import QuickTip from "$components/QuickTip.svelte";
+  import ThankYouModal from "$components/ThankYouModal.svelte";
   import toast from "svelte-french-toast";
   import { page } from "$app/stores";
   import { onMount } from "svelte";
   import { beforeNavigate, goto } from "$app/navigation";
   import { invoke } from "@tauri-apps/api/tauri";
-  import { confirm, message } from "@tauri-apps/api/dialog";
+  import { confirm } from "@tauri-apps/api/dialog";
   import { gen_uuid } from "$lib/uuids";
 
   let loaded: boolean = false;
   let uuid: string = "";
   let dont_stop: boolean = false;
   let recording: boolean = false;
+
+  // INFO: Thank You Modal Stuff
+  let modal_handle: any;
+  let message: string = "";
 
   // INFO: feedback data
   let responsiveness: number = 0;
@@ -115,6 +120,13 @@
       toast.error(`Please rate the following: ${non_rated.join(", ")}.`);
       return;
     }
+    // TODO: ensure all emotions are captured
+    if (Object.keys(emotion_data).length !== 9) {
+      toast.error(
+        "Oops, you are going too fast! Please click submit after a few seconds.",
+      );
+      return;
+    }
     try {
       console.log(emotion_data);
       console.log(data);
@@ -127,13 +139,13 @@
             : null,
         recording: recording,
       });
-      toast.success(
-        `Your feedback has been submitted successfully. ${recording ? "Recording is also stopped." : ""} You will be redirected back to the consent screen shortly.`,
-      );
+      message = `Your feedback has been submitted successfully. ${recording ? "Recording is also stopped." : ""} You will be redirected back to the consent screen shortly.`;
+      modal_handle.show();
       setTimeout(async () => {
+        modal_handle.hide();
         dont_stop = true;
         await goto("/feedback/consent");
-      }, 3000);
+      }, 4000);
     } catch (err: any) {
       toast.error(err);
     }
@@ -238,6 +250,7 @@
 </script>
 
 <UserLayout>
+  <ThankYouModal bind:this={modal_handle} {message} />
   <div class="flex flex-col h-[calc(100vh-144px)]">
     {#if loaded}
       {#if $page.data.session}

src-py/main.py
@@ -79,7 +79,7 @@ def clear_recording():
     if not fname:
         return "Missing Required Params", 400
     fname = f"{fname}.mp4"
-    is_cleared = recorder.clear_capture(fname)
+    is_cleared = faau.stop_analysis()
     if not is_cleared:
         return "Failed to clear recording", 400
     return "Recording cleared successfully"

src/components/ThankYouModal.svelte
@@ -1,0 +1,20 @@
+<script lang="ts">
+  let handle: HTMLDialogElement;
+
+  export const hide = () => {
+    handle.classList.remove("modal-open");
+  };
+
+  export const show = () => {
+    handle.classList.add("modal-open");
+  };
+
+  export let message: string;
+</script>
+
+<dialog id="confirm_model" class="backdrop-blur-md modal" bind:this={handle}>
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Thank You For Your Feedback!</h3>
+    <p class="py-4">{message}</p>
+  </div>
+</dialog>

commit fb57bdf3b6ccac74a2e1043d131d44e2aec8f58c

fix: traditional feedback blocked by throttle feat
The implemented throttling in 9af50b4 caused the traditional feedback
to be blocked because the emotion_data is empty.

src/routes/(user)/feedback/form/+page.svelte
@@ -121,7 +121,7 @@
       return;
     }
     // TODO: ensure all emotions are captured
-    if (Object.keys(emotion_data).length !== 9) {
+    if (Object.keys(emotion_data).length !== 9 && recording) {
       toast.error(
         "Oops, you are going too fast! Please click submit after a few seconds.",
       );