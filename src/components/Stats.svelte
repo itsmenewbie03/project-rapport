0f43fc341e24f7864b9c59a7163577e1a772a041
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-28 22:21:01 +0800

    feat: load parsed data instead of static values

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
   import Icon from "./Icon.svelte";
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { stats_parser } from "$lib/parser";
+  let parsed_data: Record<string, number> = {};
+  onMount(async () => {
+    const hybrid_feedback_data: string = await invoke("get_feedbacks", {
+      class: "hybrid",
+    });
+    parsed_data = stats_parser(hybrid_feedback_data);
+  });
 </script>
 
 <div class="stats shadow bg-base-200">
@@ -8,7 +18,7 @@
       <Icon name="feedback" _class="inline-block h-8 w-8 stroke-current" />
     </div>
     <div class="stat-title">Feedbacks</div>
-    <div class="stat-value">6.9K</div>
+    <div class="stat-value">{parsed_data["total"]}</div>
     <div class="stat-desc">Jan 1st - Feb 1st</div>
   </div>
 
@@ -17,7 +27,7 @@
       <Icon name="happy" _class="inline-block h-8 w-8 stroke-current" />
     </div>
     <div class="stat-title">Postive Feedbacks</div>
-    <div class="stat-value">4.2k</div>
+    <div class="stat-value">{parsed_data["positive"]}</div>
     <div class="stat-desc">Jan 1st - Feb 1st</div>
   </div>
 
@@ -26,7 +36,7 @@
       <Icon name="sad" _class="inline-block h-8 w-8 stroke-current" />
     </div>
     <div class="stat-title">Negative Feedbacks</div>
-    <div class="stat-value">2.7k</div>
+    <div class="stat-value">{parsed_data["negative"]}</div>
     <div class="stat-desc">Jan 1st - Feb 1st</div>
   </div>
 </div>
