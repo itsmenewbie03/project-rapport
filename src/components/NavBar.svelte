1dda3975166396f542cd973faf1d40222dbbd2d1
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-07-03 22:41:30 +0800

    feat(navbar): remove buttons in feedback page
    This removes the start and end buttons in the navbar component when
    the route is currently at the feedback page.

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -68,21 +68,6 @@
           <li><a href="/configurations">Configurations</a></li>
         </ul>
       </div>
-    {:else}
-      <button class="btn btn-circle btn-ghost" on:click={warn}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path></svg
-        >
-      </button>
     {/if}
   </div>
   <div class="navbar-center">
@@ -119,23 +104,6 @@
           <li><button on:click={_logout}>Logout</button></li>
         </ul>
       </div>
-    {:else}
-      <button class="btn btn-circle btn-ghost" on:click={warn}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
-      </button>
     {/if}
   </div>
 </div>
