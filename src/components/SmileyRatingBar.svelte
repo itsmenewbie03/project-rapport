1a141571e17b52fbe8d67425f142ad45e9e065ec
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-08-21 21:22:18 +0800

    feat: add new component SmileyRatingBar
    This groups SmileyRating together so we can easily manipulate rating
    bars and handle the values with less friction.

---
+++ src/components/SmileyRatingBar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import SmileyRating from "./SmileyRating.svelte";
+  export let group: number;
+</script>
+
+<div class="flex flex-row justify-between w-full">
+  <SmileyRating
+    bind:group
+    label="Very Dissatisfied"
+    value={1}
+    rating="very_dissatisfied"
+  />
+  <SmileyRating
+    bind:group
+    label="Dissatisfied"
+    value={2}
+    rating="dissatisfied"
+  />
+  <SmileyRating bind:group label="Neutral" value={3} rating="neutral" />
+  <SmileyRating bind:group label="Satisfied" value={4} rating="satisfied" />
+  <SmileyRating
+    bind:group
+    label="Very Satisfied"
+    value={5}
+    rating="very_satisfied"
+  />
+</div>
