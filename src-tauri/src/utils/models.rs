822a7675f68b5cfb43a31b8aa5491cfea6228b21
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-09-04 20:14:14 +0800

    feat: change created_at type, add DateRangeFilter
    We updated the way we store our date so we changed the corresponding
    struct as well. created_at is now u32 instead of String. DateRangeFilter
    is struct to hold the start and end date.
    
    NOTE: I wrote some lifetimes btw xD

--- src-tauri/src/utils/models.rs
+++ src-tauri/src/utils/models.rs
@@ -17,5 +17,13 @@ pub struct FeedbackDataRow {
     pub id: i32,
     pub data: String,
     pub tag: String,
-    pub created_at: String,
+    pub created_at: u32,
+}
+
+// NOTE: i wrote some lifetimes for the first time
+// because I think I undestand what I'm doing
+// do I tho?
+pub struct DateRangeFilter<'a> {
+    pub start: &'a str,
+    pub end: &'a str,
 }
