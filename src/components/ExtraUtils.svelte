6420d4a619251a6b809e138d093aba98bb3fa574
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-09-07 20:15:50 +0800

    feat: add new component ExtraUtils
    This will contain future utility features for the system, currently we
    only have archive feature. Will I work on this project after I graduate?
    If possible why not? xD

---
+++ src/components/ExtraUtils.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Icon from "./Icon.svelte";
+
+  export let handle: HTMLButtonElement;
+  export let loading: boolean = false;
+  export let click: () => void;
+</script>
+
+<ul class="menu bg-base-200 w-56 rounded-box">
+  <li class="menu-title">Utilities</li>
+  <li>
+    <button on:click={click} bind:this={handle}>
+      {#if loading}
+        <span class="loading loading-spinner loading-sm"></span>
+      {:else}
+        <Icon name="archive" _class="size-5" />
+      {/if}
+      Start Archiving
+    </button>
+  </li>
+</ul>
