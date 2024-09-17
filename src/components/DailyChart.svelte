aef49681f5278e1c0b4bfb5e9246f6eac655ff4a
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-09-17 20:34:26 +0800

    feat: add three chart components
    These components allows us to diplay charts based on what the user
    selects.

---
+++ src/components/DailyChart.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { Line } from "svelte-chartjs";
+  import { Colors } from "chart.js";
+
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+    Filler,
+  } from "chart.js";
+
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+    Filler,
+    Colors,
+  );
+
+  // NOTE: we need to cast it to
+  // any so TS would shutup xD
+  export let data;
+
+  const opts = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        min: 0.0,
+        max: 5.0,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Daily Traditional Feedback Data Chart",
+      },
+      legend: {
+        labels: {
+          usePointStyle: true,
+        },
+      },
+    },
+  };
+</script>
+
+<Line {data} options={opts} />
