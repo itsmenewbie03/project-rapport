9ed93ac5dec2a26a034a7385fd399f7df6b87b47
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-09-17 20:32:39 +0800

    feat: add rand, max, min method to FeedbackData
    This allows us to generate FeedbackData primarily used for testing.

--- src-tauri/src/utils/feedback.rs
+++ src-tauri/src/utils/feedback.rs
@@ -1,3 +1,5 @@
+use rand::distributions::{Distribution, Uniform};
+
 use crate::utils::db;
 use std::{collections::HashMap, fmt::Display, fs::File, io::Write};
 
@@ -60,6 +62,49 @@ impl FeedbackData {
         }
     }
 
+    pub fn random() -> Self {
+        let range = Uniform::from(1u8..=5u8);
+        let mut rng = rand::thread_rng();
+        FeedbackData {
+            responsiveness: range.sample(&mut rng),
+            reliability: range.sample(&mut rng),
+            access_and_facilities: range.sample(&mut rng),
+            communication: range.sample(&mut rng),
+            value_for_money: range.sample(&mut rng),
+            integrity: range.sample(&mut rng),
+            assurance: range.sample(&mut rng),
+            outcome: range.sample(&mut rng),
+            overall_satisfaction: range.sample(&mut rng),
+        }
+    }
+
+    pub fn max() -> Self {
+        FeedbackData {
+            responsiveness: 5,
+            reliability: 5,
+            access_and_facilities: 5,
+            communication: 5,
+            value_for_money: 5,
+            integrity: 5,
+            assurance: 5,
+            outcome: 5,
+            overall_satisfaction: 5,
+        }
+    }
+
+    pub fn min() -> Self {
+        FeedbackData {
+            responsiveness: 1,
+            reliability: 1,
+            access_and_facilities: 1,
+            communication: 1,
+            value_for_money: 1,
+            integrity: 1,
+            assurance: 1,
+            outcome: 1,
+            overall_satisfaction: 1,
+        }
+    }
     pub fn save_as_json(&self, id: &str) {
         let mut file =
             File::create(format!("{}_feedback.json", id)).expect("Failed to create a file!");
