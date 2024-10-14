6eda041194349e7c51478d336bfa068429612305
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-10-14 21:31:29 +0800

    feat: use mysql db for feedback data
    This moves all the feedback related data to the new central mysql db
    instead of the local sqlite db. The sqlite db will be kept and will be
    utilized to store user specific data such as services and credentials.
    
    NOTE: the connection string to mysql db is set with MYSQL_DB_URL
    envvar.

--- src-tauri/src/utils/db.rs
+++ src-tauri/src/utils/db.rs
@@ -5,15 +5,30 @@ use crate::utils::{
     feedback::FeedbackType,
     models::{ArtifactRow, ConfigData, FeedbackDataRow},
 };
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
-use tokio::runtime::Runtime;
+use dotenv::dotenv;
+use sqlx::{migrate::MigrateDatabase, MySql, MySqlPool, Sqlite, SqlitePool};
 
 use super::models::{DateRangeFilter, ServiceData};
 
 const DB_URL: &str = "sqlite://sqlite.db";
 
-async fn get_db_connection() -> Result<sqlx::Pool<Sqlite>, sqlx::Error> {
+async fn get_sqlite_db_connection() -> Result<sqlx::Pool<Sqlite>, sqlx::Error> {
     SqlitePool::connect(DB_URL).await
+}
+
+async fn get_mysql_db_connection() -> Result<sqlx::Pool<MySql>, sqlx::Error> {
+    dotenv().ok();
+    let database_url = std::env::var("MYSQL_DB_URL").expect("MYSQL_DB_URL must be set");
+    MySqlPool::connect(&database_url).await
+}
+
+#[tokio::test]
+async fn test_db_connection() {
+    init().await;
+    let db = get_mysql_db_connection().await;
+    assert!(db.is_ok());
+    let result = save_hybrid_feedback("hi crush uwu!", "TEST!").await;
+    assert!(result);
 }
 
 /// Since this is a core part of the system, we will just let this panic
@@ -24,66 +39,18 @@ pub async fn init() {
         Sqlite::create_database(DB_URL).await.unwrap();
     }
 
-    let db = get_db_connection().await.unwrap();
-    // NOTE: weird but this doesn't return any indication that the table is indeed created
+    let db = get_sqlite_db_connection().await.unwrap();
+
+    // INFO: this will initialize all the local database tables
+    // we will shadow db later to make it create tables in the central db
+
+    // INFO: start of local db init
     let _result = sqlx::query(
         "CREATE TABLE IF NOT EXISTS users (
                 id       INTEGER PRIMARY KEY NOT NULL,
                 name     VARCHAR(250) NOT NULL,
                 email    VARCHAR(250) NOT NULL UNIQUE,
                 password VARCHAR(250) NOT NULL
-        );",
-    )
-    .execute(&db)
-    .await
-    .unwrap();
-
-    // INFO: create trad_feedback_data table
-    let _result = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS trad_feedback_data (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                data        TEXT NOT NULL,
-                tag         TEXT NOT NULL,
-                created_at  INTEGER DEFAULT (strftime('%s', 'now', 'localtime'))
-        );",
-    )
-    .execute(&db)
-    .await
-    .unwrap();
-
-    // INFO: create archived_trad_feedback_data table
-    let _result = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS archived_trad_feedback_data (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                data        TEXT NOT NULL,
-                tag         TEXT NOT NULL,
-                created_at  INTEGER DEFAULT (strftime('%s', 'now', 'localtime'))
-        );",
-    )
-    .execute(&db)
-    .await
-    .unwrap();
-
-    // INFO: create hybrid_feedback_data table
-    let _result = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS hybrid_feedback_data (
-                id         INTEGER PRIMARY KEY AUTOINCREMENT,
-                data       TEXT NOT NULL,
-                tag        TEXT NOT NULL,
-                created_at INTEGER DEFAULT (strftime('%s', 'now', 'localtime'))
-        );",
-    )
-    .execute(&db)
-    .await
-    .unwrap();
-
-    // INFO: create archived_hybrid_feedback_data table
-    let _result = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS archived_hybrid_feedback_data (
-                id         INTEGER PRIMARY KEY AUTOINCREMENT,
-                data       TEXT NOT NULL,
-                tag        TEXT NOT NULL,
-                created_at INTEGER DEFAULT (strftime('%s', 'now', 'localtime'))
         );",
     )
     .execute(&db)
@@ -141,11 +108,65 @@ pub async fn init() {
         // but that way is kinda hard to tell the we're doing an early return
         return;
     }
+    // INFO: start of central db init
+    let db = get_mysql_db_connection().await.unwrap();
+    // INFO: create trad_feedback_data table
+    let _result = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS trad_feedback_data (
+                id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+                data        TEXT NOT NULL,
+                tag         TEXT NOT NULL,
+                created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );",
+    )
+    .execute(&db)
+    .await
+    .unwrap();
+
+    // INFO: create archived_trad_feedback_data table
+    let _result = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS archived_trad_feedback_data (
+                id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+                data        TEXT NOT NULL,
+                tag         TEXT NOT NULL,
+                created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );",
+    )
+    .execute(&db)
+    .await
+    .unwrap();
+
+    // INFO: create hybrid_feedback_data table
+    let _result = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS hybrid_feedback_data (
+                id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+                data        TEXT NOT NULL,
+                tag         TEXT NOT NULL,
+                created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );",
+    )
+    .execute(&db)
+    .await
+    .unwrap();
+
+    // INFO: create archived_hybrid_feedback_data table
+    let _result = sqlx::query(
+        "CREATE TABLE IF NOT EXISTS archived_hybrid_feedback_data (
+                id         INTEGER PRIMARY KEY AUTO_INCREMENT,
+                data       TEXT NOT NULL,
+                tag        TEXT NOT NULL,
+                created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );",
+    )
+    .execute(&db)
+    .await
+    .unwrap();
+
     println!("[RUST]: Database initialization completed!");
 }
 
 pub async fn get_user(email: &str) -> Option<User> {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let user_results = sqlx::query_as::<_, User>("SELECT * FROM users WHERE email=(?)")
         .bind(email)
         .fetch_all(&db)
@@ -161,7 +182,7 @@ pub async fn get_user(email: &str) -> Option<User> {
 }
 
 pub async fn get_configs() -> Option<Vec<ConfigData>> {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let configs = sqlx::query_as::<_, ConfigData>("SELECT * FROM configs;")
         .fetch_all(&db)
         .await
@@ -173,7 +194,7 @@ pub async fn get_configs() -> Option<Vec<ConfigData>> {
 }
 
 pub async fn save_configs(config: HashMap<String, String>) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     for (key, value) in config.iter() {
         let result = sqlx::query("INSERT OR REPLACE INTO configs (name, value) VALUES (?, ?)")
             .bind(key)
@@ -188,7 +209,7 @@ pub async fn save_configs(config: HashMap<String, String>) -> bool {
 }
 
 pub async fn get_services() -> Option<Vec<ServiceData>> {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let services = sqlx::query_as::<_, ServiceData>("SELECT * FROM services;")
         .fetch_all(&db)
         .await
@@ -200,7 +221,7 @@ pub async fn get_services() -> Option<Vec<ServiceData>> {
 }
 
 pub async fn add_service(service: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let result = sqlx::query("INSERT INTO services (name) VALUES (?)")
         .bind(service)
         .execute(&db)
@@ -212,7 +233,7 @@ pub async fn add_service(service: &str) -> bool {
 }
 
 pub async fn delete_service(service: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let result = sqlx::query("DELETE FROM services WHERE name = ?")
         .bind(service)
         .execute(&db)
@@ -224,7 +245,7 @@ pub async fn delete_service(service: &str) -> bool {
 }
 
 pub async fn edit_service(target: &str, update: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let result = sqlx::query("UPDATE services SET name = ? WHERE name = ?")
         .bind(update)
         .bind(target)
@@ -237,7 +258,7 @@ pub async fn edit_service(target: &str, update: &str) -> bool {
 }
 
 pub async fn update_profile(email: &str, data: HashMap<String, String>) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let user = get_user(email).await;
     match user {
         Some(_) => {
@@ -260,7 +281,7 @@ pub async fn update_profile(email: &str, data: HashMap<String, String>) -> bool 
 }
 
 pub async fn change_password(email: &str, new_password: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_sqlite_db_connection().await.unwrap();
     let password_hash = bcrypt::hash(new_password, 10).unwrap();
     let result = sqlx::query("UPDATE users SET password = (?) WHERE email = (?)")
         .bind(password_hash)
@@ -274,7 +295,7 @@ pub async fn change_password(email: &str, new_password: &str) -> bool {
 }
 
 pub async fn save_trad_feedback(data: &str, tag: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_mysql_db_connection().await.unwrap();
     let result = sqlx::query("INSERT INTO trad_feedback_data (data,tag) VALUES (?,?)")
         .bind(data)
         .bind(tag)
@@ -287,7 +308,7 @@ pub async fn save_trad_feedback(data: &str, tag: &str) -> bool {
 }
 
 pub async fn save_hybrid_feedback(data: &str, feedback_category: &str) -> bool {
-    let db = get_db_connection().await.unwrap();
+    let db = get_mysql_db_connection().await.unwrap();
     let result = sqlx::query("INSERT INTO hybrid_feedback_data (data,tag) VALUES (?,?)")
         .bind(data)
         .bind(feedback_category)
@@ -304,40 +325,29 @@ pub async fn save_hybrid_feedback_test(
     feedback_category: &str,
     created_at: &str,
 ) -> bool {
-    let db = get_db_connection().await.unwrap();
-    let result =
-        sqlx::query("INSERT INTO hybrid_feedback_data (data,tag,created_at) VALUES (?,?,?)")
-            .bind(data)
-            .bind(feedback_category)
-            .bind(created_at)
-            .execute(&db)
-            .await;
+    let db = get_mysql_db_connection().await.unwrap();
+    let result = sqlx::query(
+        "INSERT INTO hybrid_feedback_data (data,tag,created_at) VALUES (?,?,FROM_UNIXTIME(?))",
+    )
+    .bind(data)
+    .bind(feedback_category)
+    .bind(created_at)
+    .execute(&db)
+    .await;
+    dbg!(&result);
     match result {
         Ok(res) => res.rows_affected() == 1,
         Err(_) => false,
     }
 }
 
-pub fn save_hybrid_feedback_sync(data: &str, feedback_category: &str) -> bool {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        let db = get_db_connection().await.unwrap();
-        let result = sqlx::query("INSERT INTO hybrid_feedback_data (data,tag) VALUES (?,?)")
-            .bind(data)
-            .bind(feedback_category)
-            .execute(&db)
-            .await;
-        match result {
-            Ok(res) => res.rows_affected() == 1,
-            Err(_) => false,
-        }
-    })
-}
-
 pub async fn get_feedbacks(feedback_type: FeedbackType) -> Option<Vec<FeedbackDataRow>> {
     let table = feedback_type.to_string();
-    let db = get_db_connection().await.unwrap();
-    let query = format!("SELECT * FROM {}", table);
+    let db = get_mysql_db_connection().await.unwrap();
+    let query = format!(
+        "SELECT id, data, tag, UNIX_TIMESTAMP(created_at) as created_at FROM {}",
+        table
+    );
     let feedbacks = sqlx::query_as::<_, FeedbackDataRow>(&query)
         .fetch_all(&db)
         .await
@@ -353,13 +363,19 @@ pub async fn get_filtered_feedbacks(
     filter: DateRangeFilter<'_>,
 ) -> Option<Vec<FeedbackDataRow>> {
     let table = feedback_type.to_string();
-    let db = get_db_connection().await.unwrap();
-    // WARN: hey do SQL injection here please xD
-    // note idk if SQL is stupid to make my query go wrong
+    let db = get_mysql_db_connection().await.unwrap();
+    // WARN: idk if SQL is stupid to make my query go wrong
     // this is timestamp based query so yeah
     let query = format!(
-        "SELECT * FROM {} WHERE datetime(created_at,'unixepoch') BETWEEN datetime({},'unixepoch') AND datetime({},'unixepoch')",
-        // NOTE: the end will be set to the midnight of next day
+        // NOTE: this query is a bit counter intuitive,
+        // the `UNIX_TIMESTAMP` function follows the local timezone
+        // we hacked the `FROM_UNIXTIME` by subtracting 8 hours to it
+        // so we can ensure that the `start` and `end` are accurate
+        // however if we try to add 8 hours to the `UNIX_TIMESTAMP` 
+        // we will end up with an inaccurate date after parsing it with datefns
+        // so we will just leave it as it is
+        // as `datefns` properly parses the date when it's untouched (not +8 hours hack)
+        "SELECT id, data, tag, UNIX_TIMESTAMP(created_at) as created_at FROM {} WHERE created_at BETWEEN FROM_UNIXTIME({}-(8*60*60)) AND FROM_UNIXTIME({}-(8*60*60)) ORDER BY created_at;",
         table,
         filter.start,
         filter.end
@@ -369,37 +385,20 @@ pub async fn get_filtered_feedbacks(
         .fetch_all(&db)
         .await
         .unwrap();
-    println!("[RUST]: FILETERED RESULT -> {:#?}", feedbacks);
+    println!("[RUST]: TOTAL FILETERED RESULTS -> {:#?}", feedbacks.len());
+    println!("[RUST]: FIRST FILETERED RESULT -> {:#?}", feedbacks.first());
+    println!("[RUST]: LAST FILETERED RESULT -> {:#?}", feedbacks.last());
     if feedbacks.is_empty() {
         return None;
     }
     Some(feedbacks)
 }
-// #[tokio::test]
-// async fn get_artifacts_test() {
-//     let feedback_type = FeedbackType::Hybrid;
-//     let db = get_db_connection().await.unwrap();
-//     let table = match feedback_type {
-//         FeedbackType::Trad => "trad_feedback_data",
-//         FeedbackType::Hybrid => "hybrid_feedback_data",
-//     };
-//     let query = &format!(
-//         // WARN: change to 5 * 365 instead of 1
-//         "SELECT id FROM {} WHERE unixepoch('now','localtime') - created_at >= 1 * 24 * 60 * 60",
-//         table
-//     );
-//     let artifacts = sqlx::query_as::<_, ArtifactRow>(query)
-//         .fetch_all(&db)
-//         .await
-//         .unwrap();
-//     println!("[RUST]: Artifacts of {} -> {:#?}", table, artifacts);
-// }
 
 async fn get_artifacts(feedback_type: &FeedbackType) -> Option<Vec<ArtifactRow>> {
-    let db = get_db_connection().await.unwrap();
+    let db = get_mysql_db_connection().await.unwrap();
     let table = feedback_type.to_string();
     let query = &format!(
-        "SELECT id FROM {} WHERE unixepoch('now','localtime') - created_at >= 5 * 365 * 24 * 60 * 60",
+        "SELECT id FROM {} WHERE created_at <= NOW() - INTERVAL 5 YEAR",
         table
     );
     let artifacts = sqlx::query_as::<_, ArtifactRow>(query)
@@ -416,11 +415,21 @@ async fn get_artifacts(feedback_type: &FeedbackType) -> Option<Vec<ArtifactRow>>
     Some(artifacts)
 }
 
+#[tokio::test]
+async fn get_artifacts_test() {
+    init().await;
+    let artifacts = get_artifacts(&FeedbackType::Trad).await;
+    dbg!(&artifacts);
+    assert!(artifacts.is_none());
+    let artifacts = get_artifacts(&FeedbackType::Hybrid).await;
+    dbg!(&artifacts);
+    assert!(artifacts.is_some());
+}
 pub async fn archive_feedbacks() -> usize {
     let tables = [FeedbackType::Trad, FeedbackType::Hybrid];
     let mut archived_count = 0;
     for table in tables {
-        let db = get_db_connection().await.unwrap();
+        let db = get_mysql_db_connection().await.unwrap();
         let artifacts = get_artifacts(&table).await;
         if artifacts.is_none() {
             // INFO: we have nothing to do
